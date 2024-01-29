8d7fed156a35cfc24e42f210de384fc95af8c539
Bastien Clémot <bclemot85@gmail.com>
2024-01-29 10:22:07 +0100

    Update 29-01
    Solved matrix calculation (Needed to be array x vector, as it is automatically detected by numpy as a column vector); Added simulation time loop in the simulation running function.

--- CX_Script.py
+++ CX_Script.py
@@ -1,6 +1,7 @@
 #### Script for modelling and testing the CX
 ## Autor: Bastien Clémot
 ## python CX_Script.py
+## Test: python CX_Script.py -con connectivity_matrix.csv -act activity_vector.csv -t 2
 
 
 
@@ -12,17 +13,18 @@ import argparse
 
 ## ----- Import arguments with argsparse
 parser = argparse.ArgumentParser(description="Central Complexe Simulation.")
-parser.add_argument("-connectivity_file",type=str,help="Path of the file with the connectivity matrix.")
-parser.add_argument("-activity_file",type=str,help="Path of the file with the ativity vector.")
+parser.add_argument("-con",type=str,help="Path of the file with the connectivity matrix.")
+parser.add_argument("-act",type=str,help="Path of the file with the ativity vector.")
+parser.add_argument("-t",type=int,help="Simulation time.")
 args = parser.parse_args()
 
 
 
 ## ----- Import connectivity matrix and activity vector
 # Connectivity matrix
-CON_MAT = np.genfromtxt(args.connectivity_file, delimiter=',', skip_header=1)
+CON_MAT = np.genfromtxt(args.con, delimiter=',', skip_header=1)
 # Activity vector
-ACT_IN = np.genfromtxt(args.activity_file, delimiter=',', skip_header=1)
+ACT_IN = np.genfromtxt(args.act, delimiter=',', skip_header=1)
 
 # print("DEBUG - File import connectivity:")
 # print(CON_MAT)
@@ -31,6 +33,15 @@ ACT_IN = np.genfromtxt(args.activity_file, delimiter=',', skip_header=1)
 
 
 
-## ----- Function for activity transmission
-def transmission(connectivity_matrix, activity_vector):
-    return(np.dot(connectivity_matrix, activity_file))
+## ----- Final running function
+def run_simulation(connectivity_matrix, activity_vector, time):
+    
+    # Matrix multiplication
+    result = activity_vector.copy()
+    for _ in range(time):
+        result = np.matmul(connectivity_matrix, result)
+
+    return result
+
+# print("DEBUG - Post simulation:")
+# print(run_simulation(CON_MAT,ACT_IN, args.t))
